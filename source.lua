71c9cd16517f7a3d5b6d65bc4fd656c2b3c1c1b8
Emmy <83399555+Emvith@users.noreply.github.com>
2025-08-11 17:14:21 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -113,7 +113,7 @@ function Library:KeyCodeToCharacter(keyCode)
 end
 
 function Library:GetDevice()
-	if UIS.TouchEnabled and game.Players.LocalPlayer.PlayerGui:WaitForChild('TouchGui', 2) then
+	if UIS.TouchEnabled and game.Players.LocalPlayer.PlayerGui:WaitForChild('TouchGui', 3) then
 		return 'Mobile'
 	else
 		return 'PC'
